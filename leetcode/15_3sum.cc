4e129bb05344d5b3e3898919ecb394f3c770f7fb
norlanliu <liuqi.edward@gmail.com>
2015-03-11 23:23:22 +0800

    No.15 : 3 sum with two pointers

--- leetcode/15_3sum.cc
+++ leetcode/15_3sum.cc
@@ -27,11 +27,52 @@ class Solution{
 		vector<vector<int> > threeSum(vector<int> &num){
 			if(num.size() < 3)
 				return vector<vector<int>>();
+			unsigned int i;
+			int k, tmp, head, tail;
+			vector<vector<int> > tuples;
+			vector<int> tuple(3);
+			size_t size = num.size() - 1;
+			std::sort(num.begin(), num.end());
+
+			for(i = 0; i < size; ++i)
+			{
+				k = -num[i];
+				head = i + 1;
+				tail = size;
+
+				while(head < tail)
+				{
+					tmp = num[head] + num[tail];
+					if(tmp < k)
+						head++;
+					else if(tmp > k)
+						tail--;
+					else
+					{
+						tuple[0] = num[i];
+						tuple[1] = num[head];
+						tuple[2] = num[tail];
+						tuples.push_back(tuple);
+						while(head < tail && num[head] == tuple[1])
+							head++;
+						while(head < tail && num[tail] == tuple[2])
+							tail--;
+					}
+				}
+
+				while(num[i+1] == num[i])
+					i++;
+			}
+			return tuples;
+		}
+		vector<vector<int> > threeSumOld(vector<int> &num){
+			if(num.size() < 3)
+				return vector<vector<int>>();
 			unsigned int i, j;
 			int k;
 			vector<vector<int> > tuples;
 			vector<int> tuple(3);
-			unordered_map<int, unsigned int> map();
+			unordered_map<int, unsigned int> map;
 			size_t size = num.size() - 1;
 			std::sort(num.begin(), num.end());
 			
